4b7668ab4e7967f896b009a65a58293d72c26dd7
Ataher76 <ataher.cu.ms@gmail.com>
2024-07-03 21:32:02 +0600

    version e

---
+++ R/DataDescription.R
@@ -0,0 +1,25 @@
+#' CPdata
+#'
+#' This dataset contains information about the stock status Probability according to Cope & Punt, 2009.
+#'
+#' @format A data frame with 21 rows and 11 columns.
+#' @source References: Cope, J. M., & Punt, A. E. (2009). Length‐Based Reference Points for Data‐Limited Situations: Applications and Restrictions. Marine and Coastal Fisheries, 1(1), 169–186. https://doi.org/10.1577/C08-025.1
+"CPdata"
+
+
+
+#' lenfreq01
+#'
+#' This is a dummy length frequency data set used for testing purposes.
+#'
+#' @format A data frame.
+#' @source Self-generated data.
+"lenfreq01"
+
+#' lenfreq02
+#'
+#' This is another dummy length frequency data set used for testing purposes.
+#'
+#' @format A data frame.
+#' @source Self-generated data.
+"lenfreq02"
