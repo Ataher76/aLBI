8e7089ed25ec77bbfb1cfd907876d7c621822be6
Ataher76 <ataher.cu.ms@gmail.com>
2024-07-05 03:40:34 +0600

    version 0.1.3

--- vignettes/Introduction.R
+++ vignettes/Introduction.R
@@ -1,7 +1,8 @@
 ## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
+  comment = "#>",
+  cache = TRUE # Ensure caching is appropriately set
 )
 
 options(repos = c(CRAN = "https://cran.rstudio.com"))
@@ -35,15 +36,22 @@ library(aLBI)
 
 ## ----data_preparation---------------------------------------------------------
 library(readxl)
+# Load your length-frequency data from the system file
+lenfreq_path <- system.file("exdata", "LC.xlsx", package = "aLBI")
+print(lenfreq_path)  # Check the generated path
 
-# Load your length-frequency data
-data <- read_excel("D:/Academic/Fourth Year/Term paper/R package/Code/Excel/LC.xlsx", sheet = 2)
+if (lenfreq_path == "") {
+  stop("The required file LC.xlsx is missing. Please check the inst/extdata directory.")
+}
 
+# load the lenght frequency data
+lenfreq_data <- readxl::read_excel(lenfreq_path)
+print(lenfreq_data)  # check the data 
 # replace with your data directory
 
 ## ----FishPar_Example----------------------------------------------------------
 # Running the FishPar function
-results <- FishPar(data = data, resample = 1000, progress = FALSE)
+results <- FishPar(data = lenfreq_data, resample = 1000, progress = FALSE)
 
 # Viewing the results
 results$estimated_length_par
@@ -56,7 +64,15 @@ results$Pobj
 
 ## ----FishSS_Example-----------------------------------------------------------
 # Load the stock status criteria data
-cpdata <- read_excel("D:/Academic/Fourth Year/Term paper/R package/Code/Excel/PT.xlsx")
+cpdata_path <- system.file("exdata", "cpdata.xlsx", package = "aLBI")
+print(cpdata_path) #check if the path exist
+
+if (cpdata_path == "") {
+  stop("The required file cpdata.xlsx is missing. Please check the inst/extdata directory.")
+}
+# loading the cope and punt table
+cpdata <- readxl::read_excel(cpdata_path)
+print(cpdata)
 
 # Running the FishSS function
 stock_status <- FishSS(data = cpdata,
